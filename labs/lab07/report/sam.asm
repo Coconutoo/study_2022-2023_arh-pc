c400586dba66f28995bab33e2e837d9687c8e1f9
Coconutoo <115535516+Coconutoo@users.noreply.github.com>
2022-11-26 17:18:17 +0300

    Add files via upload

---
+++ labs/lab07/report/sam.asm
@@ -0,0 +1,36 @@
+%include 'in_out.asm'
+
+SECTION .data
+div: DB 'Результат: ', 0
+
+SECTION .bss
+x: RESB 80
+
+SECTION .text
+GLOBAL _start
+_start:
+
+mov ecx, x
+mov edx, 80
+call sread
+
+mov eax, x
+call atoi
+
+xor edx, edx
+
+mov ebx, 3
+div ebx
+add eax, 5
+mov ebx, 7
+mul ebx
+
+mov edi,eax
+
+
+mov eax, div
+call sprint
+mov eax, edi
+call iprintLF
+
+call quit
